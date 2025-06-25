baea54a3e47033a07d2ee38fae0716ab5a68451b
MadSc1ent1st96 <aniket.mishra200510@gmail.com>
2025-06-25 21:46:05 +0000

    Deploying to gh-pages from @ MadSc1ent1st96/MadSc1ent1st96.github.io@890c2dddabf89d18e40a4ea60fdaa0527096be01 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,12 +55,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/stellar-evolution-mesa/";
           
         },
-      },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+      },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-stellar-atlas",
