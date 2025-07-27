b7be5d48369a424aa593f5a2fc3e7481d0e560eb
MadSc1ent1st96 <aniket.mishra200510@gmail.com>
2025-07-27 07:17:41 +0000

    Deploying to gh-pages from @ MadSc1ent1st96/MadSc1ent1st96.github.io@f48017b047a64c139da06949421fcb8744babd8e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,11 +17,11 @@ ninja.data = [{
             window.location.href = "/blog/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "A growing collection of my projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/Projects/";
           },
         },{id: "nav-cv",
           title: "CV",
